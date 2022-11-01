0aa4f7eadd9d25f0828ca1a7f6cf6bba03ca8743
beltagy <moh.bell@crowdbotics.com>
2022-11-01 07:45:52 +0200

    add amadeus to requirements.txt

--- prediction/settings.py
+++ prediction/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
 
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gk$3y)w+j$#m8b=+jy5ih0f_^h4naw4(uj++s*q80b)(a&55+^'
+SECRET_KEY = env.str("SECRET_KEY",
+'django-insecure-gk$3y)w+j$#m8b=+jy5ih0f_^h4naw4(uj++s*q80b)(a&55+^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
